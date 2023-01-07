7b12b6b703cdf37fe18d0d1ced5a6752699d7b94
Sandeep Kotian <Sandeep.Kotian@veteransunited.com>
2023-01-07 17:27:48 -0600

    log added

--- Selenium_Maven/src/main/java/Pages/LoginPage.java
+++ Selenium_Maven/src/main/java/Pages/LoginPage.java
@@ -4,7 +4,6 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.Test;
 
 public class LoginPage extends BasePage {
 
